d7a323175bd4a32271bb7263b6f1d5151d977734
jeep-jim <yokkey@ya.ru>
2026-03-30 15:17:10 +0700

    remove dist folder (use Vercel build)

--- api/telegram-preview.ts
+++ api/telegram-preview.ts
@@ -381,16 +381,22 @@ function extractMessageMediaFromMessageBlock(msgHtml: string) {
     /tgme_widget_message_link_preview/i.test(msgHtml) ||
     /tgme_widget_message_webpage/i.test(msgHtml) ||
     /tgme_widget_message_site_name/i.test(msgHtml) ||
-    /tgme_widget_message_link\b/i.test(msgHtml) ||
     /tgme_widget_message_invoice/i.test(msgHtml);
+
+  const shouldIgnorePreviewImages =
+    hasWebpagePreview &&
+    !hasVideoWrap &&
+    !hasAnimationWrap &&
+    !hasAudioWrap &&
+    !hasFileWrap;
 
   const shouldBlockImageFallback =
     hasAudioWrap ||
     hasFileWrap ||
-    (hasWebpagePreview && !hasVideoWrap && !hasAnimationWrap && !hasAudioWrap && !hasFileWrap);
+    shouldIgnorePreviewImages;
 
   if (
-    hasPhotoWrap ||
+    (hasPhotoWrap && !shouldIgnorePreviewImages) ||
     hasVideoWrap ||
     hasAnimationWrap ||
     hasAudioWrap ||
