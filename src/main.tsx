365d6b46473416ab76dc3f3784861421c394b8c7
jeep-jim <yokkey@ya.ru>
2026-04-26 22:18:58 +0700

    Add GA tag

--- src/main.tsx
+++ src/main.tsx
@@ -5,43 +5,8 @@ import "./index.css";
 import { registerPwaServiceWorker } from "./lib/pwa";
 import { initTheme } from "./lib/theme";
 
-declare global {
-  interface Window {
-    dataLayer: unknown[];
-    gtag?: (...args: unknown[]) => void;
-  }
-}
-
-const GA_ID = "G-6LXS66GXHN";
-
-function initAnalytics() {
-  if (typeof window === "undefined") return;
-  if (!GA_ID) return;
-  if (document.getElementById("ga4-script")) return;
-
-  const script = document.createElement("script");
-  script.id = "ga4-script";
-  script.async = true;
-  script.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
-  document.head.appendChild(script);
-
-  window.dataLayer = window.dataLayer || [];
-
-  function gtag(...args: unknown[]) {
-    window.dataLayer.push(args);
-  }
-
-  window.gtag = gtag;
-
-  gtag("js", new Date());
-  gtag("config", GA_ID, {
-    send_page_view: true,
-  });
-}
-
 initTheme();
 registerPwaServiceWorker();
-initAnalytics();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
